aa30412aa5a4e51852bbb95ff865535824342b86
Withwws <mondoi2003@gmail.com>
2024-10-08 02:29:28 +0700

    download button alphabet

--- static/js/brands.js
+++ static/js/brands.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 const carList = document.getElementById('car-list');
+                const csvData = [];
                 carList.innerHTML = '';
                 data[letter].forEach(car => {
                     const itemCar = document.createElement('li');
@@ -17,8 +18,29 @@ document.addEventListener("DOMContentLoaded", function () {
                     carLink.href = `${encodeURIComponent(car)}`;
                     itemCar.appendChild(carLink);
                     carList.appendChild(itemCar);
+                    csvData.push(car);
                 });
+
+                // Create CSV download button
+                createCSVDownloadButton(csvData);
             })
             .catch(error => console.error('Error fetching data:', error));
     }
 });
+
+function createCSVDownloadButton(data) {
+    const csvContent = "data:text/csv;charset=utf-8," + data.map(e => e).join("\n");
+
+    const downloadLink = document.createElement("a");
+    downloadLink.href = encodeURI(csvContent);
+    downloadLink.download = `cars_${document.getElementById('selected-letter').textContent}.csv`;
+
+    // Ensure the button is selected properly
+    const downloadButton = document.querySelector(".rounded-button");
+
+    if (downloadButton) {
+        downloadButton.onclick = function () {
+            downloadLink.click(); // Trigger the download
+        };
+    }
+}
